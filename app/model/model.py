b8a09181559f8850cc533408f2ee6cb04e48c32b
Simon Larsen <swl@silvi.ai>
2024-03-09 10:18:52 +0100

    Nia

--- app/model/model.py
+++ app/model/model.py
@@ -9,6 +9,8 @@ with open( f"{BASE_DIR}/GradientBoostingClassifier-{__version__}.pkl", "rb") as 
     model = pickle.load(f)
 
 def predict_model(X):
+    print("THIS IS X:")
+    print(X)
     pred = model.predict(X)
     return pred
     
